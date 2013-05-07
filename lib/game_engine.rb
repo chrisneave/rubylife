6c263bbbcb172efb20f23749c91249e2e387a274
Chris Neave <chrispneave@gmail.com>
2013-05-07 19:52:37 +0100

    Added expectations for the GameEngine class

--- lib/game_engine.rb
+++ lib/game_engine.rb
@@ -4,15 +4,32 @@ require "my_lib"
 class GameEngine
   def self.cell_lives?(cellState, liveNeighbors)
     if liveNeighbors < 2 || liveNeighbors > 3
-      return false
+      return 0
+    end
+
+    if liveNeighbors == 3 && cellState == 0
+      return 1
     end
     
-    true
+    return 1 unless cellState == 0
+    0
   end
   
   def self.get_next_generation(currentGeneration)
     if currentGeneration.is_a?(GameState) != true
       raise ArgumentError
     end
+
+    newstate = GameState.new(currentGeneration.columns, currentGeneration.rows)
+    (0..currentGeneration.columns - 1).each do |column|
+      (0..currentGeneration.rows - 1).each do |row|
+        currentCell = currentGeneration[row][column]
+        liveNeighbors = currentGeneration.get_neighbors(column, row).select{ |x| x == 1 }.count
+        cellLives = GameEngine.cell_lives?(currentCell, liveNeighbors)
+        newstate.set_value_at_column_and_row(column, row, cellLives)
+      end
+    end
+
+    return newstate
   end
 end
---
+++ spec/gamengine_spec.rb
@@ -0,0 +1,77 @@
+# GameEngine spec
+
+require "my_lib"
+
+describe GameEngine, "#cell_lives?" do
+  it "Live cell dies if it is has less than two neighbors" do
+    # Arrange
+    # Act
+    # Assert
+    expect(GameEngine.cell_lives?(1, 0)).to eq(0)
+    expect(GameEngine.cell_lives?(1, 1)).to eq(0)
+  end
+  
+  it "Dead cel stays dead if it has less than three neighbors" do
+    # Arrange
+    # Act
+    # Assert
+    expect(GameEngine.cell_lives?(0, 0)).to eq(0)
+    expect(GameEngine.cell_lives?(0, 1)).to eq(0)
+    expect(GameEngine.cell_lives?(0, 2)).to eq(0)
+  end
+  
+  it "Live cell lives if it has two or three neighbors" do
+    # Arrange
+    # Act
+    # Assert
+    expect(GameEngine.cell_lives?(1, 2)).to eq(1)
+    expect(GameEngine.cell_lives?(1, 3)).to eq(1)
+  end
+
+  it "Live cell dies if it has more than three neighbors" do
+    # Arrange
+    # Act
+    # Assert
+    (4..9).each { |num| expect(GameEngine.cell_lives?(1, num)).to eq(0) }
+    (4..9).each { |num| expect(GameEngine.cell_lives?(0, num)).to eq(0) }
+  end
+
+  it "Dead cell lives if it has exactly three neighbors" do
+    # Arrange
+    # Act
+    # Assert
+    expect(GameEngine.cell_lives?(0, 3)).to eq(1)
+  end
+end
+
+describe GameEngine, "#get_next_generation" do
+  it "Throws ArgumentError if not a GameState object" do
+    expect { GameEngine.get_next_generation("a string") }.to raise_error(ArgumentError)
+  end
+  
+  it "Returns a new GameState object for a valid input state" do
+    # Arrange
+    gs = GameState.new(3, 3)
+    gs.set_value_at_column_and_row(1, 1, :living_cell)
+
+    # Act
+    # Assert
+    expect(GameEngine.get_next_generation(gs)).to be_a(GameState)
+  end
+  
+  it "Returns the next valid state" do
+    # Arrange
+    start = GameState.new(3, 3)
+    start[1][0] = 1
+    start[1][1] = 1
+    start[1][2] = 1
+    
+    finish = GameState.new(3, 3)
+    finish[0][1] = 1
+    finish[1][1] = 1
+    finish[2][1] = 1
+
+    # Act
+    expect(GameEngine.get_next_generation(start)).to eq(finish)
+  end
+end
